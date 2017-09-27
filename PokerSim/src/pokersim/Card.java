9147cab1646287a6aa629c945fd0849f9b2be06a
Owen <Owen@DESKTOP-98KR5L0>
2017-09-27 13:22:46 +0100

    Classes start

---
+++ PokerSim/src/pokersim/Card.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokersim;
+
+/**
+ *
+ * @author Owen
+ */
+public class Card {
+
+    private enum rank {
+        TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+    }
+
+    private enum suit {
+        DIAMONDS, CLUBS, HEARTS, SPADES
+    }
+}
