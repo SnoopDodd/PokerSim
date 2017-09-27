680b6fb41cdcecd552b3c1e647841ce1d093bde1
Owen <Owen@DESKTOP-98KR5L0>
2017-09-27 13:52:26 +0100

    Classes start

--- PokerSim/src/pokersim/Card.java
+++ PokerSim/src/pokersim/Card.java
@@ -11,11 +11,20 @@ package pokersim;
  */
 public class Card {
 
-    private enum rank {
-        TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+    public enum Rank {
+        TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(10), QUEEN(10), KING(10), ACE(11);
+
+        public static Rank[] ranks = Rank.values();
+        private int cardValue;
+        private Rank(int value) {
+            this.cardValue = value;
+        }
+        public int getValue(){
+            return cardValue;
+        }
     }
 
-    private enum suit {
+    private enum Suit {
         DIAMONDS, CLUBS, HEARTS, SPADES
     }
 }
